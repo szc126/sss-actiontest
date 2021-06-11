23e9c6a9544e1c5fd8240e4a4e57ae296f325b0c
szc126 <29127915+szc126@users.noreply.github.com>
2021-06-11 04:13:05 -0700

    Update jungno-dl.sh

--- jungno-dl.sh
+++ jungno-dl.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 wget --no-clobber "https://raw.githubusercontent.com/${GITHUB_ACTOR:?Download whose copy of sdl}/sdl/main/sdl.py" -O/tmp/sdl.py
-wget --no-clobber "https://raw.githubusercontent.com/${GITHUB_ACTOR:?Download whose copy of sdl}/sdl/main/sdl-requirements.txt" -O/tmp/sdl.py
+wget --no-clobber "https://raw.githubusercontent.com/${GITHUB_ACTOR:?Download whose copy of sdl}/sdl/main/sdl-requirements.txt" -O/tmp/sdl-requirements.txt
 pip install -r /tmp/sdl-requirements.txt
 chmod +x /tmp/sdl.py
 /tmp/sdl.py "ko:重刊老乞大諺解 001.pdf" -o "/tmp/sdl/jungno-a" &
