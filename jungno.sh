d21f81693327477f90a66f277a56bd9b1448a111
szc126 <29127915+szc126@users.noreply.github.com>
2021-06-11 03:45:58 -0700

    Update jungno.sh

--- jungno.sh
+++ jungno.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-python3 jungno.py /tmp/sdl/jungno-{a,b}/* -c text > /tmp/jungno.txt &
-python3 jungno.py /tmp/sdl/jungno-{a,b}/* -c html > /tmp/jungno.htm; dragon /tmp/jungno.htm &
+chmod +x jungno.py
+./jungno.py /tmp/sdl/jungno-{a,b}/* -c text > /tmp/jungno.txt &
+./jungno.py /tmp/sdl/jungno-{a,b}/* -c html > /tmp/jungno.htm; dragon /tmp/jungno.htm &
